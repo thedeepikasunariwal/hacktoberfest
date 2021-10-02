42173486bf9179299a1344d4569c3b074c1c34d9
Aditya Saini <65843082+akki-cyber@users.noreply.github.com>
2021-10-02 12:37:58 +0530

    Create printnumber.cpp

---
+++ printnumber.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int number;
+
+    cout<< "Enter an integer";
+    cin >> number;
+    cout << " Entered number" << number;
+    return 0;
+}
