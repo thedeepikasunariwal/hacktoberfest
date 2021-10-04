9085a3bcb54c95193c5adb8afcd826c0847c5156
thedeepikasunariwal <91868814+thedeepikasunariwal@users.noreply.github.com>
2021-10-04 12:09:49 +0530

    Create Evenodd.C

---
+++ Evenodd.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+  int n,i;
+  printf("enter the number");
+  scanf("%d",&n);
+  i=0;
+  if(i=n%2)
+  {
+    printf("the number is odd");
+  }
+  else
+  {
+    printf("the numbar is even");
+  }
+}
